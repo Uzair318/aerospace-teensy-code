bdd290d20c95664aeabdc1446fe09ecf3330d6ff
Uzair Ahmed <Uzair318@vt.edu>
2021-02-12 19:39:05 -0500

    V0.1 Tx and Rx code written. Deploying and testing

--- CAN_blink_receiver/CAN_blink_receiver.ino
+++ CAN_blink_receiver/CAN_blink_receiver.ino
@@ -5,17 +5,18 @@
  */
 #include <FlexCAN_T4.h>
 FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> can1; 
-const ledPin = LED_BUILTIN;
+const int ledPin = LED_BUILTIN;
 
 void setup() {
   Serial.begin(115200); delay(400);
   can1.begin();
   can1.setBaudRate(250000);
   can1.setMB(MB11,RX,STD);
-  can1.onReceive(MB11, canSniff)
+  can1.onReceive(MB11, canSniff);
 }
 
-void canSniff(const CANFD_message_t &msg) {
+void canSniff(const CAN_message_t &msg) {
+  digitalWrite(ledPin, HIGH);
   Serial.print("MB "); Serial.print(msg.mb);
   Serial.print("  OVERRUN: "); Serial.print(msg.flags.overrun);
   Serial.print("  LEN: "); Serial.print(msg.len);
@@ -26,6 +27,7 @@ void canSniff(const CANFD_message_t &msg) {
   for ( uint8_t i = 0; i < msg.len; i++ ) {
     Serial.print(msg.buf[i], HEX); Serial.print(" ");
   } Serial.println();
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
