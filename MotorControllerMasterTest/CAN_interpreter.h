7f0ffd11f7ac341043ee78e50f0094f28e8b9537
Uzair Ahmed <Uzair318@vt.edu>
2021-03-28 11:16:41 -0400

    using Alex's CAN interpreter library

---
+++ MotorControllerMasterTest/CAN_interpreter.h
@@ -0,0 +1,25 @@
+/*
+    CAN interpreter library to take user input and create Flexcan4 messages for Maxon EPOS4 controller
+    Alex Broz
+*/
+#ifndef CAN_interpreter_h
+#define CAN_interpreter_h
+
+#include "Arduino.h"
+#include "FlexCAN_T4.h"
+#include <stdlib.h>
+
+class CAN_interpreter
+{
+  public:
+    CAN_interpreter();
+    uint8_t createMsg(char *input_ptr,CAN_message_t *msg_ptr);
+    void interpretMsg(CAN_message_t *msg_ptr);
+  private:
+    //char _input[32];
+    CAN_message_t _msg;
+    char _temp[33];
+    uint8_t _length = 0;
+    uint32_t _data;
+};
+#endif
