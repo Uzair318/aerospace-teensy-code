21a3ae941acbde5a9d86b159781b1193b1a69955
Uzair Ahmed <Uzair318@vt.edu>
2021-03-28 10:48:46 -0400

    adding commands to test motor

--- MotorControllerMasterTest/MotorControllerMasterTest.ino
+++ MotorControllerMasterTest/MotorControllerMasterTest.ino
@@ -12,9 +12,12 @@ void setup() {
     can1.enableFIFO();
     can1.enableFIFOInterrupt();
     can1.onReceive(FIFO, canSniff);
-    delay(1000);
+    
+//    delay(1000);
     Serial.println("CAN setup finished");
-    delay(500);
+//    delay(500);
+
+
 }
 
 void loop() {
@@ -25,21 +28,171 @@ void loop() {
             Serial.println("Sending ...");
             CAN_message_t msg;
             msg.len = 8;
+            // Write «Target position» (Index 0x607A, Subindex 0x00: Data 0x000008AE  2222dec) to node 1:
+            msg.id = 0x601;  // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x22;    // ccs = 2 (read object)
+            msg.buf[1] = 0x7A;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0x00;
+            msg.buf[5] = 0x7D;
+            msg.buf[6] = 0x00;
+            msg.buf[7] = 0x00;
+
+            can1.write(msg);
+            c = 'z';
+        } else if(c == 'b') {
+            Serial.println("Sending ...");
+            CAN_message_t msg;
+            msg.len = 8;
+            // Read «Position actual value» (Index 0x6064, Subindex 0x00) from node 1:
+            msg.id = 0x601;  // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x40;    // ccs = 2 (read object)
+            msg.buf[1] = 0x64;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0x00;
+            msg.buf[5] = 0x00;
+            msg.buf[6] = 0x00;
+            msg.buf[7] = 0x00;
+            can1.write(msg);
+            c = 'z';
+        } else if(c == 's') {
+            Serial.println("Sending setup command");
+            CAN_message_t msg;
+            msg.len = 8;
+            // 
             msg.id = 0x0601;  // 0x600 + node_id (SDO client to server)
-            msg.buf[0] = 40;    // ccs = 2 (read object)
-            msg.buf[1] = 0x18;  // index low byte
-            msg.buf[2] = 0x10;  // index high byte
-            msg.buf[3] = 0x01;  // subindex
+            msg.buf[0] = 0x22;    // ccs = 1 (write object)
+            msg.buf[1] = 0x40;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0b1011;//0x0F;
+            msg.buf[5] = 0;
+            msg.buf[6] = 0;
+            msg.buf[7] = 0;
+            can1.write(msg);
+
+            Serial.println("Check status of controller");
+            msg.len = 8;
+            // 
+            msg.id = 0x0601;      // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x40;    // ccs = 2 (read object)
+            msg.buf[1] = 0x41;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0;     // subindex
             msg.buf[4] = 0;
             msg.buf[5] = 0;
             msg.buf[6] = 0;
             msg.buf[7] = 0;
-
             can1.write(msg);
-            c = 'b';
+        } else if(c == 'x') {
+            Serial.println("Sending SHUTDOWN command");
+            CAN_message_t msg;
+            msg.len = 8;
+            // 
+            msg.id = 0x0601;  // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x22;    // ccs = 1 (write object)
+            msg.buf[1] = 0x40;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0;
+            msg.buf[5] = 0;
+            msg.buf[6] = 0;
+            msg.buf[7] = 0;
+            can1.write(msg);
+            
+            Serial.println("Check status of controller");
+            msg.len = 8;
+            // 
+            msg.id = 0x0601;      // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x40;    // ccs = 2 (read object)
+            msg.buf[1] = 0x41;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0;     // subindex
+            msg.buf[4] = 0;
+            msg.buf[5] = 0;
+            msg.buf[6] = 0;
+            msg.buf[7] = 0;
+            can1.write(msg);
+        } else if(c == 'v') {
+            Serial.println("Sending Velocity Demand Value");
+            CAN_message_t msg;
+            msg.len = 8;
+//            // Send Velocity Demand Value
+//            msg.id = 0x601;  // 0x600 + node_id (SDO client to server)
+//            msg.buf[0] = 0x22;    // ccs = 1 (write object)
+//            msg.buf[1] = 0x6B;  // index low byte
+//            msg.buf[2] = 0x60;  // index high byte
+//            msg.buf[3] = 0x00;  // subindex
+//            msg.buf[4] = 0x64;
+//            msg.buf[5] = 0x00;
+//            msg.buf[6] = 0x00;
+//            msg.buf[7] = 0x00;
+//            can1.write(msg);
+//            c = 'z';
+            // Send Velocity Demand Value
+            msg.id = 0x601;  // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x22;    // ccs = 1 (write object)
+            msg.buf[1] = 0xFF;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0xE8;
+            msg.buf[5] = 0x03;
+            msg.buf[6] = 0x00;
+            msg.buf[7] = 0x00;
+            can1.write(msg);
+            c = 'z';
+        } else if(c == 'r') {
+            Serial.println("Sending ...");
+            CAN_message_t msg;
+            msg.len = 8;
+            // Get Velocity Actual Value
+            msg.id = 0x601;  // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x40;    // ccs = 2 (read object)
+            msg.buf[1] = 0x6C;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0x00;
+            msg.buf[5] = 0x00;
+            msg.buf[6] = 0x00;
+            msg.buf[7] = 0x00;
+            can1.write(msg);
+            c = 'z';
+        } else if(c == 'm') {
+            Serial.println("Sending request for Mode of Operation");
+            CAN_message_t msg;
+            msg.len = 8;
+            // Mode of Operation
+            msg.id = 0x601;  // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x40;    // ccs = 2 (read object)
+            msg.buf[1] = 0x61;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0x00;
+            msg.buf[5] = 0x00;
+            msg.buf[6] = 0x00;
+            msg.buf[7] = 0x00;
+            can1.write(msg);
+            c = 'z';
+        } else if(c == 'n') {
+            Serial.println("Setting Mode of Operation to Velocity");
+            CAN_message_t msg;
+            msg.len = 8;
+            // Mode of Operation
+            msg.id = 0x601;  // 0x600 + node_id (SDO client to server)
+            msg.buf[0] = 0x22;    // ccs = 2 (read object)
+            msg.buf[1] = 0x60;  // index low byte
+            msg.buf[2] = 0x60;  // index high byte
+            msg.buf[3] = 0x00;  // subindex
+            msg.buf[4] = 0x03;
+            msg.buf[5] = 0x00;
+            msg.buf[6] = 0x00;
+            msg.buf[7] = 0x00;
+            can1.write(msg);
+            c = 'z';
         }
     }
-//    delay(100);
 }
 
 void canSniff(const CAN_message_t &msg) {
